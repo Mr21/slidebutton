32932e712375c440186a5e9a54dce47d2e876b60
Timer91 <marais.jeremie@gmail.com>
2015-10-03 01:42:23 +0200

    Fixed red progress bar issue, set height auto, released code

--- slidebutton.js
+++ slidebutton.js
@@ -4,68 +4,80 @@
 */
 
 $.element( {
-	name: "togglebutton",
+	name: "slidebutton",
 	htmlReplace:
-		'<div class="togglebutton">'+
-			'<span class="btn"></span>'+
+		'<div class="slidebutton">'+
+			'<div class="slider"></div>'+
+			'<span class="thumb"></span>'+
 		'</div>'
 	,
 	css: '\
-		.togglebutton {\
-			overflow: hidden;\
+		.slidebutton {\
 			display: inline-block;\
 			position: relative;\
-			width: 35px;\
-			height: 14px;\
+			min-width: 20px;\
+			min-height: 10px;\
+			max-height: 20px;\
 			cursor: pointer;\
-			border-radius: 10px;\
-			background-color: rgba(0, 0, 0, 0.33);\
 		}\
-		.togglebutton:before {\
-			content: "";\
-			position: absolute;\
-			top: 0;\
-			left: 0;\
-			width: 7px;\
-			height: 100%;\
-			background-color: #f33;\
-			border-radius: 10px;\
-		}\
-		.active.togglebutton:before {\
-			width: 100%;\
-		}\
-		.btn {\
-			display: inline-block;\
-			position: absolute;\
-			width: 14px;\
-			height: 100%;\
-			left: 0;\
-			border-radius: 50%;\
-			background-color: rgba(255, 255, 255, 1);\
-			box-shadow: inset 0px 0px 0px 1px rgba(0, 0, 0, 0.11);\
-		}\
-		.togglebutton:before,\
-		.btn {\
-			transition: all .3s;\
-		}\
+			.slider {\
+				overflow: hidden;\
+				position: relative;\
+				width: 100%;\
+				background-color: rgba(0, 0, 0, 0.33);\
+			}\
+				.slider:before {\
+					content: "";\
+					left: 0;\
+					width: 7px;\
+					background-color: #f33;\
+				}\
+				.slider,\
+				.slider:before {\
+					height: 100%;\
+					border-radius: 10px;\
+				}\
+				.active.slidebutton .slider:before {\
+					width: 100%;\
+				}\
+			.thumb {\
+				display: inline-block;\
+				height: 100%;\
+				left: -1px;\
+				border-radius: 50%;\
+				background-color: rgba(255, 255, 255, 1);\
+			}\
+				.slider,\
+				.thumb {\
+					top: 0;\
+				}\
+				.slider:before,\
+				.thumb {\
+					position: absolute;\
+					transition: all .3s;\
+				}\
 	',
 	init: function() {
 		var
 			jqElement = this.jqElement,
-			thumb = jqElement.children( ".btn" ),
+			slider = jqElement.children( ".slider" ),
+			thumb = jqElement.children( ".thumb" ),
+			shift = 1,
 			active = false
 		;
 
+		// thumb initialization with slide button height
+		thumb.css( { "width" : jqElement.height() } );
+		slider.css
+
 		jqElement
 			.click( function() {
-				var moveLeft = jqElement.width() - thumb.width();
+				var moveLeft = jqElement.width() - thumb.width() + shift;
 
-				thumb.css({
-					"left" : active ? 0 : moveLeft
-				});
+				thumb.css( { "left" : active ? -shift : moveLeft } );
 
 				active = !active
-				jqElement.toggleClass("active");
+				jqElement.toggleClass( "active" );
 			});
 
 	}
