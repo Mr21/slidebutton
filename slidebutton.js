9628e58797189bf52f102414b81ec9eed2d30554
Thomas Tortorini <thomastortorini@gmail.com>
2015-10-03 05:35:03 +0200

    slidebutton is now a checkbox

--- slidebutton.js
+++ slidebutton.js
@@ -3,10 +3,13 @@
 	https://github.com/jquery-element/slidebutton
 */
 
+function lg(s) {console.log(s)}
+
 $.element( {
 	name: "slidebutton",
 	htmlReplace:
 		'<div class="slidebutton">'+
+			'{{html}}'+
 			'<div class="slidebutton-track">'+
 				'<div class="slidebutton-on"></div>'+
 			'</div>'+
@@ -14,6 +17,9 @@ $.element( {
 		'</div>'
 	,
 	css: '\
+		.slidebutton input {\
+			display: none;\
+		}\
 		.slidebutton {\
 			display: inline-block;\
 			position: relative;\
@@ -59,6 +65,7 @@ $.element( {
 			jqElement = this.jqElement,
 			jqThumb = jqElement.children( ".slidebutton-thumb" ),
 			jqOn = jqElement.find( ".slidebutton-on" ),
+			elCheckbox = jqElement.children( "input" )[ 0 ],
 			thumbH = jqThumb.height(),
 			active = false,
 			elemW
@@ -67,19 +74,24 @@ $.element( {
 		jqThumb.css( "width", thumbH );
 		jqOn.css( "width", thumbH / 2 );
 
-		jqElement.click( function() {
+		function click() {
 			thumbH = jqThumb.height();
-
 			if ( active = !active ) {
+				elCheckbox.setAttribute( "checked", "checked" );
 				elemW = jqElement.width();
 				jqOn.css( "width", elemW - thumbH / 2 );
 				jqThumb.css( "left", elemW - thumbH );
 			} else {
+				elCheckbox.removeAttribute( "checked" );
 				jqOn.css( "width", thumbH / 2 );
 				jqThumb.css( "left", 0 );
 			}
-
 			jqElement.toggleClass( "slidebutton-active", active );
-		});
+		}
+
+		jqElement.click( click );
+		if ( elCheckbox.checked ) {
+			click();
+		}
 	}
 });
