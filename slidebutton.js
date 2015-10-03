2e52f9d44c531393c3e633642e57dddcfe267f90
Thomas Tortorini <thomastortorini@gmail.com>
2015-10-03 03:55:24 +0200

    coding style

--- slidebutton.js
+++ slidebutton.js
@@ -1,14 +1,14 @@
 /*
 	slidebutton - 1.0.0
-	https://github.com/Timer91/slidebutton
+	https://github.com/jquery-element/slidebutton
 */
 
 $.element( {
 	name: "slidebutton",
 	htmlReplace:
 		'<div class="slidebutton">'+
-			'<div class="slider"></div>'+
-			'<span class="thumb"></span>'+
+			'<div class="slidebutton-track"></div>'+
+			'<span class="slidebutton-thumb"></span>'+
 		'</div>'
 	,
 	css: '\
@@ -20,71 +20,63 @@ $.element( {
 			max-height: 20px;\
 			cursor: pointer;\
 			-webkit-touch-callout: none;\
-		    -webkit-user-select: none;\
-		     -khtml-user-select: none;\
-		       -moz-user-select: none;\
-		        -ms-user-select: none;\
-		    		user-select: none;\
+			-webkit-user-select: none;\
+			   -moz-user-select: none;\
+			    -ms-user-select: none;\
+			        user-select: none;\
 		}\
-			.slider {\
-				overflow: hidden;\
-				position: relative;\
-				width: 100%;\
-				background-color: rgba(0, 0, 0, 0.33);\
-			}\
-				.slider:before {\
-					content: "";\
-					left: 0;\
-					width: 7px;\
-					background-color: #f33;\
-				}\
-				.slider,\
-				.slider:before {\
-					height: 100%;\
-					border-radius: 10px;\
-				}\
-				.active.slidebutton .slider:before {\
-					width: 100%;\
-				}\
-			.thumb {\
-				display: inline-block;\
-				height: 100%;\
-				left: -1px;\
-				border-radius: 50%;\
-				background-color: rgba(255, 255, 255, 1);\
-			}\
-				.slider,\
-				.thumb {\
-					top: 0;\
-				}\
-				.slider:before,\
-				.thumb {\
-					position: absolute;\
-					transition: all .3s;\
-				}\
+		.slidebutton-track {\
+			overflow: hidden;\
+			position: relative;\
+			width: 100%;\
+			background-color: rgba( 0, 0, 0, .33 );\
+		}\
+		.slidebutton-track:before {\
+			content: "";\
+			left: 0;\
+			width: 7px;\
+			background-color: #f33;\
+		}\
+		.slidebutton-track,\
+		.slidebutton-track:before {\
+			height: 100%;\
+			border-radius: 10px;\
+		}\
+		.slidebutton-active .slidebutton-track:before {\
+			width: 100%;\
+		}\
+		.slidebutton-thumb {\
+			display: inline-block;\
+			height: 100%;\
+			left: -1px;\
+			border-radius: 50%;\
+			background-color: #fff;\
+		}\
+		.slidebutton-track,\
+		.slidebutton-thumb {\
+			top: 0;\
+		}\
+		.slidebutton-track:before,\
+		.slidebutton-thumb {\
+			position: absolute;\
+			transition: all .3s;\
+		}\
 	',
 	init: function() {
 		var
 			jqElement = this.jqElement,
-			slider = jqElement.children( ".slider" ),
-			thumb = jqElement.children( ".thumb" ),
-			shift = 1,
+			jqThumb = jqElement.children( ".slidebutton-thumb" ),
 			active = false
 		;
 
-		// thumb initialization with slide button height
-		thumb.css( { "width" : jqElement.height() } );
-		slider.css
+		jqThumb.css( "width", jqElement.height() );
 
-		jqElement
-			.click( function() {
-				var moveLeft = jqElement.width() - thumb.width() + shift;
-
-				thumb.css( { "left" : active ? -shift : moveLeft } );
-
-				active = !active
-				jqElement.toggleClass( "active" );
-			});
-
+		jqElement.click( function() {
+			jqThumb.css(
+				"left",
+				active ? -1 : jqElement.width() - jqThumb.width() + 1
+			);
+			jqElement.toggleClass( "slidebutton-active", active = !active );
+		});
 	}
 });
